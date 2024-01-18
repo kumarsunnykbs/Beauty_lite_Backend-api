3e696121096ee7013ae60fae2455bd6399e1354a
Sunny <47320462+kumarsunnykbs@users.noreply.github.com>
2024-01-18 17:21:30 +0530

    Update db.js

--- app/model/db.js
+++ app/model/db.js
@@ -1,30 +1,8 @@
 /*'user strict';
 
-var mysql = require('mysql');
 
-//local mysql db connection
-var connection = mysql.createConnection({
-    host     : 'uzyth.cyqw8btngqoz.us-east-2.rds.amazonaws.com',
-    user     : 'adminuzyth',
-    password : 'jG9OzAyB2oR5dUt3ojnz',
-    database : 'db_uzyth'
-});
-
-connection.connect(function(err) {
-	 console.log("err",err);
-    if (err) throw err;
-    console.log("connected with mysql!!");
-});
-
-module.exports = connection;*/
 
 const mysql = require("mysql");
-/* 
-$dbhost = 'localhost';
- $dbuser = 'root';
- $dbpass = '09UzL423Ffs3fo4SecreLogQn1ndbshawrvsjaqwe761';
- $dbname = 'db_uzyth'; 
-*/
 
 module.exports = mysql.createPool({
   host: "localhost",
@@ -33,9 +11,4 @@ module.exports = mysql.createPool({
   database: "beauty_lite_db",
 });
 
-/* createConnection module.exports = mysql.createPool({
-    host     : 'uzlatest.cyqw8btngqoz.us-east-2.rds.amazonaws.com',
-    user     : 'adminuzyth',
-    password : '0508SecuRtgbTns67sq18hd5hs2sh1gg',
-    database : 'db_uzyth'
-}) */
+
